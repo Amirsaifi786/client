803be8345656614484fc2649fd6c1ce787c39183
unknown <bcanikki@gmail.com>
2026-03-13 12:42:05 +0530

    work

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import API, { IMAGE_URL } from "../api/axios";
 import { Link } from "react-router-dom";
 import Testimonial from './Testimonial';
 import FeaturedProperty from './FeaturedProperty';
+import Regions from "../components/Regions";
 function Home() {
 
   const [locations, setLocations] = useState([]);
@@ -97,52 +98,7 @@ function Home() {
       </div>
 
       {/* POPULAR REGIONS */}
-      <div className="region-section py-5">
-        <div className="container">
-          <h2 className="text-white text-center mb-5 h3">Discover Rooms, Flats & Perfect Living Spaces</h2>
-
-          <div className="row g-4">
-
-            {locations.map((city) => (
-
-              <div className="col-md-4" key={city.id}>
-
-
-
-                <Link
-                  to={`/location/${slugify(city.title)}`}
-                  className="img-box d-block position-relative shadow-sm rounded-3 overflow-hidden"
-                  style={{
-                    backgroundImage: `url(${IMAGE_URL}/${city.image})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    height: "250px"
-                  }}
-                >
-
-                  <div className="img-box-overlay position-absolute top-0 start-0 w-100 h-100"></div>
-
-                  <div
-                    className="img-box-content position-absolute bottom-0 start-0 p-4 text-white w-100"
-                    style={{ zIndex: 2 }}
-                  >
-
-                    <h4 className="fw-bold mb-0">{city.title}</h4>
-
-                    <span className="small opacity-75">
-                      {city.available} Properties Available
-                    </span>
-
-                  </div>
-
-                </Link>
-              </div>
-
-            ))}
-
-          </div>
-        </div>
-      </div>
+      <Regions />
 
       {/* FEATURED PROPERTIES */}
       <FeaturedProperty properties={properties} />
