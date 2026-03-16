c8a59717dd62311f7005e310838be4aba37c963d
unknown <bcanikki@gmail.com>
2026-03-16 15:11:01 +0530

    work

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,17 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+// export const IMAGE_URL = "http://localhost:5000/uploads";
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://room-dekho-backend-5.onrender.com/api",
 });
-export const IMAGE_URL = "http://localhost:5000/uploads";
+export const IMAGE_URL = "https://room-dekho-backend-5.onrender.com/uploads";
 
 export default API;
